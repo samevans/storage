06cc6e2f6d901291ac1ebed74c6861eb9c9f96df
Sam Evans <samevans@protonmail.com>
2017-09-11 13:34:54 -0400

    contact us/thank you

--- storage/src/mvp_landing/urls.py
+++ storage/src/mvp_landing/urls.py
@@ -9,6 +9,8 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'signups.views.home', name='home'),
+    url(r'^thank-you/$', 'signups.views.thankyou', name='thankyou'),
+    url(r'^contact-us/$', 'signups.views.contactus', name='contactus'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
