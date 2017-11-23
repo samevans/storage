8973a974b7e2fa8569d10fc76182baadb9918f65
Sam Evans <smlevans@gmail.com>
2017-11-23 16:45:38 -0500

    build error fix

--- storage/src/wizrobe/forms.py
+++ storage/src/wizrobe/forms.py
@@ -14,8 +14,6 @@ class RegistrationForm(UserCreationForm):
     def save(self, commit=True):
         user = super(RegistrationForm, self).save(commit=False)
         user.username = self.cleaned_data['username']
-        user.first_name = self.cleaned_data['first_name']
-        user.last_name = self.cleaned_data['last_name']
         user.email = self.cleaned_data['email']
         if commit:
             user.save()
