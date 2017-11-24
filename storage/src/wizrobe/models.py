7ad288e990d85c2d43dfabb09a206242bb1e68c0
Sam Evans <smlevans@gmail.com>
2017-11-24 14:25:07 -0500

    Annoying image errors... profile image init

--- storage/src/wizrobe/models.py
+++ storage/src/wizrobe/models.py
@@ -1,12 +1,16 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.signals import post_save
+from PIL import Image
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
     zipcode = models.CharField(max_length=120, default='')
     city = models.CharField(max_length=120, default='')
     phone = models.IntegerField(default=0)
+    image = models.ImageField(upload_to='profile_image', blank=True)
+    def __str__(self):
+        return self.user.username
     
 def create_profile(sender, **kwargs):
     if kwargs['created']:
